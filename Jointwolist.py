92e835a4d6fca62117fb33328fecd6b57ce6bebe
Anzim Tasrif <109681624+anzimtasrif@users.noreply.github.com>
2025-01-28 19:07:32 +0600

    Add files via upload

---
+++ Jointwolist.py
@@ -0,0 +1,17 @@
+#Join Lists 
+
+num1 = [1,2,3]
+num2 = [4,5,6]
+
+num1.extend(num2)
+
+print(num1)
+
+#Python Join Lists 
+
+num1 = ["A","B","C"]
+num2 = [4,5,6]
+
+num1.extend(num2)
+
+print(num1)
